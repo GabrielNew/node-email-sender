8262bd41dc6b390017cba183cc393c5404cdf2b3
GabrielNew <gabrielfai70@gmail.com>
2025-01-12 18:20:05 -0300

    Send email function completed and test file

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const nodemailer = require("nodemailer");
+const { USER_EMAIL, USER_PASSWORD } = process.env;
 
 const transporter = nodemailer.createTransport({
-  host: "",
-  port: 465,
+  host: "smtp.gmail.com",
+  port: 587,
   secure: false, // true for port 465, false for other ports
   auth: {
-    user: "",
-    pass: "",
+    user: USER_EMAIL,
+    pass: USER_PASSWORD,
   },
 });
 
 async function sendEmail() {
   const info = await transporter.sendMail({
-    from: "",
-    to: "",
-    subject: "",
-    text: "Hello world?",
-    html: "<b>Hello world?</b>",
+    from: USER_EMAIL,
+    to: ["email1@test.com", "email2@test.com"],
+    subject: "Test email with Nodemailer, Nodejs and Attachments",
+    text: "Hello world",
+    html: "<b>Hello world?</b><p>Nodemailer with Nodejs</p>",
+    attachments: [
+      {
+        filename: "test.txt",
+        path: "C:YOUR_PATH/email-sender/testAttachment.txt",
+      },
+    ],
   });
 
   console.log("Message sent: %s", info.messageId);
